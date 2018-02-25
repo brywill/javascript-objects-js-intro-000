523dfb6c2951ac6a0a880bcea5b660f7888a63f5
Bryan Williams <brywilldev@gmail.com>
2018-02-25 19:46:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,11 @@
 var playlist = { tool: undertow };
+
+function updatePlaylist(playlist, artist, song) {
+  playlist.artist = song
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist.artist
+  return playlist
+}
